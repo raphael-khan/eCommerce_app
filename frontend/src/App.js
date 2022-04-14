37c15aff8f32437aab37c58dd3602812310a0b50
raphael-khan <raphaelkhan@yahoo.com>
2022-04-13 20:56:12 -0400

    created and styled header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
     <>
       < Header />
-      <main>
-        <h1>Welcome to Wired !</h1>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Wired !</h1>
+        </Container>
       </main>
       < Footer />
     </>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+
 
 const Header = () => {
   return (
     <header>
-        Header
+      <Navbar className='navbar-dark bg-primary' expand='lg'>
+        <Container>
+          <Navbar.Brand href="/">Wired !</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">< i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+              <Nav.Link href="/login">< i className='fas fa-user'></i> Sign In</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
 
     
