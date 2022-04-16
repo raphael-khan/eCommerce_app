c0ac4b5060bc1623943eefc91c2dbe34ca211480
raphael-khan <raphaelkhan@yahoo.com>
2022-04-16 14:21:36 -0400

    optimized performence and used links, created productscreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { Container } from "react-bootstrap"
 import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
           </Routes>
         </Container>
       </main>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,32 +1,46 @@
-import React from 'react'
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
-
+import React from "react"
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap"
+import { LinkContainer } from "react-router-bootstrap"
 
 const Header = () => {
   return (
     <header>
       <Navbar className='navbar-dark bg-primary' expand='lg' sticky='top'>
         <Container>
-          <Navbar.Brand href="/">Wired !</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <LinkContainer to='/'>
+            <Navbar.Brand>Wired !</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href="/cart">< i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-              <Nav.Link href="/login">< i className='fas fa-user'></i> Sign In</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
-          </Nav>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
+              <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+                <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.2'>
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.3'>
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='#action/3.4'>
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-
-    
   )
 }
 
