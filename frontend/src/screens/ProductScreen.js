d7e05b3c29b6568f33e471e87611bdcb8d2c9ce2
raphael-khan <raphaelkhan@yahoo.com>
2022-04-16 15:27:11 -0400

    created the product screen and redered products

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,77 @@
 import React from "react"
+import { Link, useParams } from "react-router-dom"
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from "react-bootstrap"
+import Rating from "../components/Rating"
+import products from "../products"
 
 const ProductScreen = () => {
-  return <div>Product</div>
+  const params = useParams()
+  const product = products.find((p) => p._id === params.id)
+
+  return (
+    <>
+      <Link className='btn btn-warning my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroupItem>
+              <h4>{product.name}</h4>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroupItem>
+            <ListGroupItem>Price: ${product.price}</ListGroupItem>
+          </ListGroup>
+          <ListGroupItem>Description : {product.description}</ListGroupItem>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroupItem>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0
+                      ? "In Stock"
+                      : "Sorry, out of stock. Please check back"}
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button className='btn-block btn-success' type='button'>
+                  Add To Cart
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default ProductScreen
