1a36d651bbf83485990dd5d456af706290e28ce3
raphael-khan <raphaelkhan@yahoo.com>
2022-04-18 13:50:37 -0400

    created user and product models for the DB

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: string,
+      required: true,
+    },
+    email: {
+      type: string,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: string,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = mongoose.model("User", userSchema)
+
+export default User
