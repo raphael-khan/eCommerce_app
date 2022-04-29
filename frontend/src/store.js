1cfcf59335f34b36751899c29778580c3250699d
raphael-khan <raphaelkhan@yahoo.com>
2022-04-28 20:47:26 -0400

    created consts for productReducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,15 +5,20 @@ import {
 } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
+import { productListReducer } from "./reducers/productReducers"
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  productList: productListReducer,
+})
 
 const initialState = {}
+
+const middleware = [thunk]
 
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(...middleware))
 )
 
 export default store
