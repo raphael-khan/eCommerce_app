f5cb5dbc5bde7158756f34a9e17d4bb42538d51c
raphael-khan <raphaelkhan@yahoo.com>
2022-04-28 22:32:19 -0400

    updates

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import productRoutes from "./routes/productRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 dotenv.config()
 
@@ -13,25 +14,11 @@ app.get("/", (req, res) => {
   res.send("API is running")
 })
 
-// mount it.
+// Mount it.
 app.use("/api/products", productRoutes)
 
-// error handler.
-
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`)
-  res.status(404)
-  next(error)
-})
-
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-  res.status(statusCode)
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  })
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
