686414607af140f555f10da9aa04b19217f64e50
raphael-khan <raphaelkhan@yahoo.com>
2022-05-01 12:53:54 -0400

    created ShippingScreen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,13 +1,32 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import { Form, Button, Row, Col } from "react-bootstrap"
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { Form, Button } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
-import Message from "../components/Message"
-import Loader from "../components/Loader"
 import FormContainer from "../components/FormContainer"
 
 const ShippingScreen = () => {
-  return <div>ShippingScreen</div>
+  const [address, setAddress] = useState("")
+  const [city, setCity] = useState("")
+  const [zipcode, setZipcode] = useState("")
+  const [country, setCountry] = useState("")
+
+  return (
+    <FormContainer>
+      <h4>Shipping</h4>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='address'>
+          <Form.Label>Address:</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter address'
+            value={address}
+            required
+            onChange={(e) => setAddress(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+      </Form>
+    </FormContainer>
+  )
 }
 
 export default ShippingScreen
