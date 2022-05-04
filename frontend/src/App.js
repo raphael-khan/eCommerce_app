ca1728cf7b583ac5775193b8178b4c1f6141a733
raphael-khan <raphaelkhan@yahoo.com>
2022-05-03 21:09:19 -0400

    added payment method to the PlaceorderScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import CartScreen from "./screens/CartScreen"
 import ShippingScreen from "./screens/ShippingScreen"
 import PaymentScreen from "./screens/PaymentScreen"
+import PlaceOrderScreen from "./screens/PlaceOrderScreen"
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
             <Route path='/cart/:id' element={<CartScreen />} />
             <Route path='/shipping' element={<ShippingScreen />} />
             <Route path='/payment' element={<PaymentScreen />} />
+            <Route path='/placeorder' element={<PlaceOrderScreen />} />
           </Routes>
         </Container>
       </main>
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,28 +1,39 @@
-import React from 'react'
-import { Button. Row, Col, ListGroup, Image, Card } from "react-bootstrap"
+import React from "react"
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
-import Message from '../components/Message'
+import Message from "../components/Message"
 import CheckoutSteps from "../components/CheckoutSteps"
 
 const PlaceOrderScreen = () => {
-  const cart = useSelector(state => state.cart)
+  const cart = useSelector((state) => state.cart)
 
-    return (
+  return (
     <>
-        <CheckoutSteps step1 step2 step3 step4 />
-        <Row>
-            <Col md={8}>
-                <ListGroup variant='flush'>
-                    <ListGroup.Item>
-                        <h4>Shipping</h4>
-                        <p>
-                            <strong>Address:</strong>
-                            {cart.shippingAddress.address}
-                        </p>
-                    </ListGroup.Item>
-                </ListGroup>
-            </Col>
-        </Row>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h4>Shipping</h4>
+              <p>
+                <strong>Address: </strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city},
+                {cart.shippingAddress.zipCode}, {cart.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h4>Payment Method</h4>
+              <strong>Payment Method: </strong>
+              {cart.paymentMethod}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h4>Payment Method</h4>
+              <strong>Payment Method: </strong>
+              {cart.paymentMethod}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
     </>
   )
 }
