51c4d1c3c852796b74192d667ec3104f72adff8d
raphael-khan <raphaelkhan@yahoo.com>
2022-05-07 14:42:23 -0400

    updates to  the userController send user info back

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,7 +31,19 @@ const authUser = asyncHandler(async (req, res) => {
 //@access  Private.
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("Success")
+  const user = await User.findById(req.user._id) //current logged in user.
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
 })
 
 export { authUser, getUserProfile }
