331611c12af034158632e64ba45ec0488c443f5e
raphael-khan <raphaelkhan@yahoo.com>
2022-05-07 11:53:22 -0400

    addeed genrate token to userController

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,5 +1,11 @@
 import jwt from "jsonwebtoken"
 
+// Generates a JWT token and assigns a payload to it along with the signature / secret
+
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_TOKEN)
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  })
 }
+
+export default generateToken
