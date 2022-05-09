66185a130d604193ad72bf1e12281eb97b8e3a0a
raphael-khan <raphaelkhan@yahoo.com>
2022-05-08 22:56:26 -0400

    added registration to userActions

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,9 +3,13 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from "../constants/userConstants"
 import axios from "axios"
 
+// User login action.
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
@@ -45,4 +49,44 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo")
   dispatch({ type: USER_LOGOUT })
+}
+
+// User register action.
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    })
+    // config object to send in the token in the header
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+    const { data } = await axios.post(
+      "/api/users",
+      { name, email, password },
+      config
+    )
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    })
+    //logging in the user right after registration.
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
+
+    // Setting the user registration to local storage
+    localStorage.setItem("userInfo", JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
