51c224ab43d3e461d0fe0e9bc1816d8f2f4c817d
raphael-khan <raphaelkhan@yahoo.com>
2022-05-16 21:47:48 -0400

    started working on the orderScreen

---
+++ frontend/src/screens/orderScreen.js
@@ -0,0 +1,120 @@
+import React, { useEffect } from "react"
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap"
+import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+import Message from "../components/Message"
+import Loader from "../components/CheckoutSteps"
+import { getOrderDetails } from "../actions/orderActions"
+
+const orderScreen = ({ match }) => {
+  const orderId = match.params.id
+  const dispatch = useDispatch()
+
+  const orderDetails = useSelector((state) => state.orderDetails)
+  const { order, loading, error } = orderDetails
+  
+  if(!loading) {}
+      order.itemsPrice = order.orderItems.reduce(
+        (acc, item) => acc + item.price * item.qty,
+        0)
+  }
+
+  useEffect(() => {
+    dispatch(getOrderDetails(orderId))
+  }, [orderId, dispatch])
+
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant='danger'>{error}</Message>
+  ) : (
+    <>
+      <h1>Order {order._id}</h1>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h4>Shipping</h4>
+              <p>
+                <strong>Address: </strong>
+                {order.shippingAddress.address}, {order.shippingAddress.city},
+                {order.shippingAddress.zipCode}, {order.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h4>Payment Method</h4>
+              <strong>Payment Method: </strong>
+              {order.paymentMethod}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h4>Order Items</h4>
+              {order.orderItems.length === 0 ? (
+                <Message>Order is empty</Message>
+              ) : (
+                <ListGroup variant='flush'>
+                  {order.orderItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={2}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x ${item.price} = ${item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h4>Order Summary</h4>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items Price:</Col>
+                  <Col>${order.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping & Handling:</Col>
+                  <Col>${order.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax:</Col>
+                  <Col>${order.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total:</Col>
+                  <Col>${order.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default orderScreen
